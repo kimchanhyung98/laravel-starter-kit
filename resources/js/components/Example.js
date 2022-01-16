8f56d5d2881b4f561c4efcd3d5a2af2fff4efa07
kimchanhyung98 <kimchanhyung98@gmail.com>
2022-01-16 22:01:33 +0900

    Update Example.js
    - Example에서 App으로 변경

--- resources/js/components/Example.js
+++ resources/js/components/Example.js
@@ -1,24 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function Example() {
+function App() {
     return (
-        <div className="container">
-            <div className="row justify-content-center">
-                <div className="col-md-8">
-                    <div className="card">
-                        <div className="card-header">Example Component</div>
-
-                        <div className="card-body">I'm an example component!</div>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <div>Example Component</div>
     );
 }
 
-export default Example;
+export default App;
 
-if (document.getElementById('example')) {
-    ReactDOM.render(<Example />, document.getElementById('example'));
+if (document.getElementById('app')) {
+    ReactDOM.render(<App />, document.getElementById('app'));
 }
