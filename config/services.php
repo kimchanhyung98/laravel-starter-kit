029fba282cce19a1015505a4caaef07d84da522e
kimchanhyung98 <kimchanhyung98@gmail.com>
2022-02-05 23:25:14 +0900

    Add socialite

--- config/services.php
+++ config/services.php
@@ -30,4 +30,10 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
+    'kakao' => [
+        'client_id' => env('KAKAO_CLIENT_ID'),
+        'client_secret' => env('KAKAO_CLIENT_SECRET'),
+        'redirect' => env('KAKAO_REDIRECT_URI')
+    ],
+
 ];
