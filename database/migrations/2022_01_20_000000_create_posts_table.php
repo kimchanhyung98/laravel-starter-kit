d108d938028239145bc29398beafe7bd3c380a29
kimchanhyung98 <kimchanhyung98@gmail.com>
2022-04-03 20:39:49 +0900

    Update Laravel 9.x
    - laravel 9.x 업데이트
    - php 8.1 업데이트
    - 기존 리액트 제거

--- database/migrations/2022_01_20_000000_create_posts_table.php
+++ database/migrations/2022_01_20_000000_create_posts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePostsTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -16,7 +16,7 @@ class CreatePostsTable extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained();
-            $table->enum('type', ['notice', 'faq'])->comment('게시글 종류');
+            $table->enum('type', ['notice', 'faq', 'free'])->comment('게시글 종류');
 
             $table->string('name')->comment('제목');
             $table->text('content')->comment('내용');
@@ -37,4 +37,4 @@ class CreatePostsTable extends Migration
     {
         Schema::dropIfExists('posts');
     }
-}
+};
--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::view('{path}', 'app')->where('path', '.*');
+Route::get('/', function () {
+    return view('welcome');
+});
