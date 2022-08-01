405c861799a6ce27594bf5c662d2f16865ac0b27
kimchanhyung98 <kimchanhyung98@gmail.com>
2022-08-01 09:19:53 +0900

    Update Packages

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,25 +2,11 @@
 
 namespace App\Models;
 
-use Eloquent;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use Illuminate\Database\Query\Builder;
 
-/**
- * App\Models\Post
- *
- * @property-read User|null $user
- * @method static \Illuminate\Database\Eloquent\Builder|Post newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Post newQuery()
- * @method static Builder|Post onlyTrashed()
- * @method static \Illuminate\Database\Eloquent\Builder|Post query()
- * @method static Builder|Post withTrashed()
- * @method static Builder|Post withoutTrashed()
- * @mixin Eloquent
- */
 class Post extends Model
 {
     use HasFactory;
