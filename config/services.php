6d76e0908ae98f25dab5c7a63ce607cc44eacdb0
kimchanhyung98 <kimchanhyung98@gmail.com>
2023-10-23 00:12:05 +0900

    Install socialite
    update socialite&app config

--- config/services.php
+++ config/services.php
@@ -31,4 +31,15 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
+    // Socialite
+    'apple' => [
+        'client_id' => env('APPLE_CLIENT_ID'),
+        'client_secret' => env('APPLE_CLIENT_SECRET'),
+        'redirect' => env('APPLE_REDIRECT_URI'),
+    ],
+    'kakao' => [
+        'client_id' => env('KAKAO_CLIENT_ID'),
+        'client_secret' => env('KAKAO_CLIENT_SECRET'),
+        'redirect' => env('KAKAO_REDIRECT_URI'),
+    ],
 ];
