00a24213b8ceca4f6997624a31c95b9d95d150b8
kimchanhyung98 <kimchanhyung98@gmail.com>
2023-10-23 00:33:02 +0900

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,8 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['*'],
+    // 'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
