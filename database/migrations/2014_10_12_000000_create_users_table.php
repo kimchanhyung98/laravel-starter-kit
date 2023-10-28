d724e34f74673aa66c057f1357a9b95cf610c61c
kimchanhyung98 <kimchanhyung98@gmail.com>
2023-10-28 22:33:29 +0900

    Create User models (socialite)

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name', 100);
             $table->string('nickname', 50)->nullable();
-            $table->string('email', 100)->unique();
+            $table->string('email', 200)->nullable()->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('phone', 20)->nullable();
 
