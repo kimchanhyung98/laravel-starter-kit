3cd79a1bb05646e2ec699e9d468940b3cf4247f5
kimchanhyung98 <kimchanhyung98@gmail.com>
2024-04-02 15:54:06 +0900

    Update PostController, exception

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,6 +7,7 @@ use App\Http\Resources\Post\IndexResource;
 use App\Http\Resources\Post\MessageResource;
 use App\Http\Resources\Post\ShowResource;
 use App\Models\Post;
+use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 use Illuminate\Support\Facades\Auth;
@@ -18,8 +19,8 @@ class PostController extends Controller
     {
         return IndexResource::collection(
             Post::search($request->q)
-            // ->when($request->type, fn ($query, $type) => $query->where('type', $type))
-            // ->query(fn (Builder $query) => $query->with('user:id,nickname'))
+                // ->when($request->type, fn ($query, $type) => $query->where('type', $type))
+                // ->query(fn (Builder $query) => $query->with('user:id,nickname'))
                 ->when($request->type, function ($query, $type) {
                     return $query->where('type', $type);
                 })->query(function ($query) {
@@ -30,14 +31,19 @@ class PostController extends Controller
 
     public function store(Request $request): MessageResource
     {
-        $post = Post::create([
-            'user_id' => Auth::id(),
-            'type' => $request->type ?? null,
-            'title' => $request->title,
-            'contents' => $request->contents,
-            'is_open' => $request->is_open ?? false,
-        ]);
-        $post->searchable();
+        try {
+            $post = Post::create([
+                'user_id' => Auth::id(),
+                'type' => $request->type ?? null,
+                'title' => $request->title,
+                'contents' => $request->contents,
+                'is_open' => $request->is_open ?? false,
+            ]);
+            $post->searchable();
+        } catch (Exception $e) {
+            logger($e->getMessage());
+            abort(500);
+        }
 
         return new MessageResource([
             'id' => $post->id,
@@ -47,7 +53,11 @@ class PostController extends Controller
 
     public function show(Post $post): ShowResource
     {
-        $post->increment('hit');
+        try {
+            $post->increment('hit');
+        } catch (Exception $e) {
+            logger($e->getMessage());
+        }
 
         return new ShowResource($post);
     }
@@ -63,12 +73,18 @@ class PostController extends Controller
     {
         Gate::authorize('update', $post);
 
-        $post->update([
-            'type' => $request->type ?? null,
-            'title' => $request->title,
-            'contents' => $request->contents,
-            'is_open' => $request->is_open ?? false,
-        ]);
+        try {
+            $post->update([
+                'type' => $request->type,
+                'title' => $request->title,
+                'contents' => $request->contents,
+                'is_open' => $request->is_open,
+            ]);
+            $post->searchable();
+        } catch (Exception $e) {
+            logger($e->getMessage());
+            abort(500);
+        }
 
         return new MessageResource([
             'id' => $post->id,
@@ -79,7 +95,14 @@ class PostController extends Controller
     public function destroy(Post $post): MessageResource
     {
         Gate::authorize('delete', $post);
-        $post->delete();
+
+        try {
+            $post->delete();
+            $post->unsearchable();
+        } catch (Exception $e) {
+            logger($e->getMessage());
+            abort(500);
+        }
 
         return new MessageResource([
             'id' => 0,
