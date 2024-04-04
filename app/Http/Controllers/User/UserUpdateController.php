8a5138975e28aba3e6ec28221a08e20e4ca19f11
kimchanhyung98 <kimchanhyung98@gmail.com>
2024-04-04 17:28:04 +0900

    Update user edit

--- app/Http/Controllers/User/UserUpdateController.php
+++ app/Http/Controllers/User/UserUpdateController.php
@@ -8,20 +8,27 @@ use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UserUpdateController extends Controller
 {
-    public function __invoke(Request $request)
+    /**
+     * 회원 정보 수정
+     */
+    public function __invoke(Request $request): MessageResource
     {
         $user = Auth::user();
-        
+        if (! $user) {
+            abort(401, __('user.unauthorized'));
+        }
+
         try {
             DB::beginTransaction();
             $user->update([
                 'name' => $request->name,
                 'nickname' => $request->nickname,
                 'email' => $request->email,
-                // 'password' => $request->password ? bcrypt($request->password) : $user->password,
+                // 'password' => $request->password ? Hash::make($request->password) : $user->password,
             ]);
             DB::commit();
         } catch (Exception $e) {
