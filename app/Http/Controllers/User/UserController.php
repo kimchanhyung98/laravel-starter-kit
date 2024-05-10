e2b525020380b6f86b3e8d5961cf7c3a1575da42
kimchanhyung98 <kimchanhyung98@gmail.com>
2024-05-10 14:33:05 +0900

    Update UserResource directory

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
-use App\Http\Resources\Account\UserResource;
+use App\Http\Resources\User\UserResource;
 use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
