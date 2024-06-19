b5763f9d613efbda213a5e1b761616f66267aa07
kimchanhyung98 <kimchanhyung98@gmail.com>
2024-06-19 19:36:21 +0900

    Update PostShowTest.php

--- tests/Feature/Post/PostShowTest.php
+++ tests/Feature/Post/PostShowTest.php
@@ -15,7 +15,7 @@ class PostShowTest extends TestCase
 
     private function sendPostShow(int $postId): TestResponse
     {
-        return $this->getJson('api/posts/'.$postId);
+        return $this->getJson('api/posts/' . $postId);
     }
 
     public function test_post_show_success(): void
@@ -31,13 +31,32 @@ class PostShowTest extends TestCase
             ]]);
     }
 
-    public function test_user_show_fail_with_unpublished_post(): void
+    public function test_post_show_success_for_owner_with_unpublished_post(): void
     {
-        Sanctum::actingAs(User::factory()->create());
+        $user = User::factory()->create();
+        Sanctum::actingAs($user);
 
-        $post = Post::factory()->create(['is_published' => false]);
+        $post = Post::factory()->create([
+            'user_id' => $user->id,
+            'is_published' => false
+        ]);
 
-        logger($post);
+        $this->sendPostShow($post->id)
+            ->assertOk()
+            ->assertJsonStructure(['data' => [
+                'id', 'user_name', 'type', 'title', 'contents', 'hit', 'like_count', 'comment_count', 'created_at', 'is_liked', 'is_editable',
+            ]]);
+    }
+
+    public function test_user_show_fail_with_forbidden(): void
+    {
+        // Sanctum::actingAs(User::factory()->create());
+
+        $post = Post::factory()->create([
+            'user_id' => User::factory()->create()->id,
+            'is_published' => false
+        ]);
+
         $this->sendPostShow($post->id)
             ->assertForbidden();
     }
